f9fdc8651bcb75293936dc5827dd0944eca12b4e
Monique Vith <z5310404@ad.unsw.edu.au>
2024-01-17 15:11:52 +1100

    added logos for skills

--- personal-portfolio-website/src/components/AboutMe.jsx
+++ personal-portfolio-website/src/components/AboutMe.jsx
@@ -48,17 +48,40 @@ function AboutMe() {
                     </ul>
                 </div>
 
-                <div className="MEA-flex-box">
-                    <div className="MEA-header">
-                        <div className="MEA-logo"></div>
-                        <div className="MEA-title">Robogals UNSW</div>
+                <div className="Robo-flex-box">
+                    <div className="Robo-header">
+                        <div className="Robo-logo"></div>
+                        <div className="Robo-title">Robogals UNSW</div>
                     </div>
-                    <div className="MEA-role">Executive Marketing Manager</div>
-                    <ul className="MEA-role-descrriptions">
-                        <li className="MEA-description">Managed a sub-committee marketing team in creating content to advertise and promote Robogals events and brand in aims to encourage women in STEM fields. </li>
-                        <li className="MEA-description">Robogals has over 600+ members in the society </li>
+                    <div className="Robo-role">Executive Marketing Manager</div>
+                    <ul className="Robo-role-descrriptions">
+                        <li className="Robo-description">Managed a sub-committee marketing team in creating content to advertise and promote Robogals events and brand in aims to encourage women in STEM fields. </li>
+                        <li className="Robo-description">Robogals has over 600+ members in the society </li>
                     </ul>
                 </div>
+            </div>
+
+            <div className="skills-group-header">
+                <div className="separator"></div>
+                <div className="skills-header">My Skills</div>
+            </div>
+
+            <div className="skills-categories">
+                <button className="programming-button">Programming</button>
+                <button className="frameworks-button">Frameworks & Tools</button>
+                <button className="personal-skills-button">Personal Skills</button>
+            </div>
+            <div className="skills-descriptions">
+                <ul className="programming-qualifications">
+                    <li className="skills-text python-logo">Python</li>
+                    <li className="skills-text js-logo">Javascript</li>
+                    <li className="skills-text c-logo">C</li>
+                </ul>
+                <ul className="programming-qualifications">
+                    <li className="skills-text sql-logo">SQL/PosgreSQL</li>
+                    <li className="skills-text html-logo">HTML</li>
+                    <li className="skills-text css-logo">CSS</li>
+                </ul>
             </div>
         </>
     )
