4087d8c83b24a990ef80305856f234572ef31c15
Monique Vith <z5310404@ad.unsw.edu.au>
2024-01-22 13:02:00 +1100

    added content for contact me page

--- personal-portfolio-website/src/components/ContactMe.jsx
+++ personal-portfolio-website/src/components/ContactMe.jsx
@@ -1,9 +1,32 @@
 import React from "react";
 
+import '../ContactMe.css'
+
 function ContactMe() {
     return (
-        <>
-            contact me page
+        <>  
+            <div className="contactme-section">
+                <div className="header">Contact me!</div>
+                <div className="enquiries-section">
+                    <div className="online-enquiry-section">
+                        <h2 id="online-enq-header">Online Enquiry</h2>
+                        <textarea placeholder="Name" type="text" className="input-online-enq"></textarea>
+                        <textarea placeholder="Email" type="text" className="input-online-enq"></textarea>
+                        <textarea placeholder="Phone Number" type="numbers" className="input-online-enq"></textarea>
+                        <textarea placeholder="Message" type="text" className="input-online-enq message-input"></textarea>
+                    </div>
+                    <div className="contact-details-section">
+                        <h2 id="contact-details-header">Contact Details</h2>
+                        <h3 className="contact-info">Email</h3>
+                        <p className="contact-info">moniquevith1@gmail.com</p>
+                        <h3 className="contact-info">Phone Number</h3>
+                        <p className="contact-info">0466 372 776</p>
+                        <h3 className="contact-info">Social Media</h3>
+                        <a className="contact-info" href="https://www.linkedin.com/in/moniquevith/">https://www.linkedin.com/in/moniquevith/</a>
+                        <a className="contact-info" href="https://github.com/moniquevith">https://github.com/moniquevith</a>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
