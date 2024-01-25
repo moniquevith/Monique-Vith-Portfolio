17ec6af6f8f403a7e6e6a7861c1bc06e436f2aac
Monique Vith <z5310404@ad.unsw.edu.au>
2024-01-25 16:27:01 +1100

    added tictactoe project

--- personal-portfolio-website/src/components/PersonalProjects.jsx
+++ personal-portfolio-website/src/components/PersonalProjects.jsx
@@ -24,7 +24,7 @@ function PersonalProjects() {
                     <div className="separator"></div>
                     <p className="personal-project-description">The primary aim of my personal portfolio website is to showcase my existing profile and skills as a student in software engineering. It features sections dedicated to my personal projects, an "About Me" segment, and a designated area for inquiries. Future implementations include a personal blog section.</p>
                     <div className="languages-title">Languages: <span className="languages-text">JavaScript, HTML, CSS</span></div>
-                    <div className="frameworks-title">Frameworks:  <span className="frameworks-text">ReactJS, Figma, VScode, NodeJS/Express, Nodemailer</span></div>
+                    <div className="frameworks-title">Tools & Frameworks:  <span className="frameworks-text">ReactJS, Figma, VScode, NodeJS/Express, Nodemailer</span></div>
                     <div className="pp-button-section">
                         <button className="pp-buttons" onClick={() => DownloadReport(WebsiteReport)}>Download Report</button>
                         <Link className="seecode-button" to="https://github.com/moniquevith/Personal-Portfolio-Website">See Code</Link>
@@ -32,26 +32,15 @@ function PersonalProjects() {
                 </div>
             </div>
             <div className="personal-projects-section">
-                <div className="calculator-image"></div>
+                <div className="tic-tac-toe-image"></div>
                 <div className="personal-website-content">
-                    <h2 className="personal-website-header">Calculator Application</h2>
-                    <p className="personal-project-description">Description of the project here. Description of the project here. Description of the project here. Description of the project here. Description of the project here. Description of the project here. Description of the project here. </p>
-                    <div className="languages-title">Languages: <span className="languages-text">JavaScript, HTML/CSS</span></div>
-                    <div className="frameworks-title">Frameworks:  <span className="frameworks-text">ReactJS, MaterialUI .</span></div>
+                    <h2 className="personal-website-header">Tic Tac Toe Game</h2>
+                    <div className="separator"></div>
+                    <p className="personal-project-description">Tic-tac-toe is a game for two players who take turns marking the spaces in a three-by-three grid with X or O. The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row is the winner. </p>
+                    <div className="languages-title">Languages: <span className="languages-text">Python</span></div>
+                    <div className="frameworks-title">Tools & Frameworks:  <span className="frameworks-text">VScode</span></div>
                     <div className="pp-button-section">
-                        <button className="seecode-button">See Code</button>
-                    </div>
-                </div>
-            </div>
-            <div className="personal-projects-section">
-                <div className="lol-image"></div>
-                <div className="personal-website-content">
-                    <h2 className="personal-website-header">Some sort of Game</h2>
-                    <p className="personal-project-description">Description of the project here. Description of the project here. Description of the project here. Description of the project here. Description of the project here. Description of the project here. Description of the project here. </p>
-                    <div className="languages-title">Languages: <span className="languages-text">JavaScript, HTML/CSS</span></div>
-                    <div className="frameworks-title">Frameworks:  <span className="frameworks-text">ReactJS, MaterialUI .</span></div>
-                    <div className="pp-button-section">
-                        <button className="seecode-button">See Code</button>
+                        <Link className="seecode-button" to="https://github.com/moniquevith/TicTacToe">See Code</Link>
                     </div>
                 </div>
             </div>
