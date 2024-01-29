b0d6104ba13111f5f66f80d5098ef3d30eb738c9
Monique Vith <z5310404@ad.unsw.edu.au>
2024-01-29 12:35:41 +1100

    added navigate

--- personal-portfolio-website/src/PageList.jsx
+++ personal-portfolio-website/src/PageList.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import './App.css'
 
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import AboutMe from "./components/AboutMe";
 import Navigation from "./components/Navigation";
 import PersonalProjects from "./components/PersonalProjects";
@@ -16,7 +16,7 @@ function PageList() {
                 <br />
                 <Routes>
                     <Route path="/" element={<AboutMe />} />
-                    <Route path="/Monique-Vith-Portfolio" element={<AboutMe />} />
+                    <Route path="/Monique-Vith-Portfolio" element={<Navigate to="/" replace />} />
                     <Route path="/personal/projects" element={<PersonalProjects />} />
                     <Route path="/blogs" element={<Blogs />} />
                     <Route path="/contact/me" element={<ContactMe />} />
